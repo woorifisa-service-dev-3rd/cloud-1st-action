fd166eb1e230d8b5c58ac605c0da8fc669c44480
seonmin5 <seonmin5555@gmail.com>
2024-10-10 17:33:18 +0900

    test: 통합 테스트1:

--- frontend-3rd-loan/app/page.tsx
+++ frontend-3rd-loan/app/page.tsx
@@ -4,8 +4,15 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <Image src="/images/logo.jpeg" alt="로고 이미지" width={400} height={400} />
-      <h1 className="text-5xl text-blue-900 font-bold mt-10">우리 FISA 캐피탈</h1>
+      <Image
+        src="/images/logo.jpeg"
+        alt="로고 이미지"
+        width={400}
+        height={400}
+      />
+      <h1 className="text-5xl text-blue-900 font-bold mt-10">
+        우리 FISA 캐피탈 hi
+      </h1>
     </div>
   );
 };
